5fe967fbcca1aa45466f027a11c58416511d67eb
github-ci <you@example.com>
2025-11-03 14:18:33 +0800

    update++++++++++++

--- src/CounterBundle.php
+++ src/CounterBundle.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
 use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\EasyAdminMenuBundle\EasyAdminMenuBundle;
 
 class CounterBundle extends Bundle implements BundleDependencyInterface
 {
@@ -16,6 +17,7 @@ class CounterBundle extends Bundle implements BundleDependencyInterface
             DoctrineBundle::class => ['all' => true],
             DoctrineIndexedBundle::class => ['all' => true],
             DoctrineTimestampBundle::class => ['all' => true],
+            EasyAdminMenuBundle::class => ['all' => true],
         ];
     }
 }
